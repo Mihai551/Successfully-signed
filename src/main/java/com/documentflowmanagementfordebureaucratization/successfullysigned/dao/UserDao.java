c3ca73d224de02076d5c713d2b23980aa0437936
Mihai551 <mihairadu999@gmail.com>
2022-04-04 23:29:48 +0300

    .

--- src/main/java/com/documentflowmanagementfordebureaucratization/successfullysigned/dao/UserDao.java
+++ src/main/java/com/documentflowmanagementfordebureaucratization/successfullysigned/dao/UserDao.java
@@ -4,8 +4,8 @@ import com.documentflowmanagementfordebureaucratization.successfullysigned.entit
 
 public interface UserDao {
 
-    public User findByUserName(String userName);
-    
-    public void save(User user);
-    
+	public User findByUserName(String userName);
+
+	public void save(User user);
+
 }
